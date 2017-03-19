024b990e86696ccce99070c27d4af65640be4b43
Chris Dorst <chris@dorst.email>
2017-03-19 09:34:35 -0500

    Adding CodeGeneration.AddNugetFeed function

---
+++ Automation.Functions.CodeGeneration.AddNugetFeed/Process.cs
@@ -0,0 +1,22 @@
+﻿using Automation.Functions.Abstractions;
+using System;
+using System.IO;
+
+namespace Automation.Functions.CodeGeneration.AddNugetFeed
+{
+    public class Process : IFunction
+    {
+        public Func<dynamic, dynamic, dynamic> Function { get; }
+            = (state, process) =>
+            {
+                var path = (string)process["path"];
+                if (!path.EndsWith("nuget.config"))
+                {
+                    path = Path.Combine(path, "nuget.config");
+                }
+                state = new AddNugetConfig.Process().Function(state, new { path = path });
+                state = new FileFindAndReplace.Process().Function(state, new { path = path, oldText = "</packageSources>", newText = $"<add key=\"{(string)process["name"]}\" value=\"{(string)process["value"]}\" /></packageSources>" });
+                return state;
+            };
+    }
+}
