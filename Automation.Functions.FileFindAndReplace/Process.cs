b7edfa25867b3bede6d8e5e9611199fab2a73da8
Chris Dorst <chris@dorst.email>
2017-03-19 08:58:13 -0500

    Adding FileFindAndReplace

---
+++ Automation.Functions.FileFindAndReplace/Process.cs
@@ -0,0 +1,19 @@
+﻿using Automation.Functions.Abstractions;
+using System;
+
+namespace Automation.Functions.FileFindAndReplace
+{
+    public class Process : IFunction
+    {
+        public Func<dynamic, dynamic, dynamic> Function { get; }
+            = (state, process) =>
+            {
+                var key = Guid.NewGuid().ToString();
+                state = new ReadFile.Process().Function(state, new { key = key, path = process["path"] });
+                state = new Replace.Process().Function(state, new { key = key, oldText = process["oldText"], newText = process["newText"] });
+                state = new WriteFile.Process().Function(state, new { key = key, path = process["path"] });
+                state[key] = null;
+                return state;
+            };
+    }
+}
