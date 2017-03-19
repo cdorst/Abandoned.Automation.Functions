f8d91385ffe42c028f857744636580c472183d3b
Chris Dorst <chris@dorst.email>
2017-03-19 08:49:34 -0500

    Adding Replace function

---
+++ Automation.Functions.Replace/Process.cs
@@ -0,0 +1,28 @@
+﻿using Automation.Functions.Abstractions;
+using System;
+
+namespace Automation.Functions.Replace
+{
+    public class Process : IFunction
+    {
+        public Func<dynamic, dynamic, dynamic> Function { get; }
+            = (state, process) =>
+            {
+                var key = (string)process["key"];
+                if (string.IsNullOrWhiteSpace(key))
+                {
+                    throw new ArgumentNullException("key",
+                        "The key property must be defined for the current ReadFile process. This is the key used to save the file contents as string to the state object");
+                }
+                var oldText = (string)process["oldText"];
+                if (string.IsNullOrWhiteSpace(oldText))
+                {
+                    throw new ArgumentNullException("oldText",
+                        "The oldText property must be defined for the current ReadFile process. This is the text to find and replace.");
+                }
+                var newText = (string)process["newText"];
+                state[key] = ((string)state[key])?.Replace(oldText, newText);
+                return state;
+            };
+    }
+}
