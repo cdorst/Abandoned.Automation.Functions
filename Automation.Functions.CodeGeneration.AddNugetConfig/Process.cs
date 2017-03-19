203ac2e3830100c54e418d62af0a42f499de3e71
Chris Dorst <chris@dorst.email>
2017-03-19 09:19:54 -0500

    Adding CodeGeneration.AddNugetConfig function

---
+++ Automation.Functions.CodeGeneration.AddNugetConfig/Process.cs
@@ -0,0 +1,32 @@
+﻿using Automation.Functions.Abstractions;
+using System;
+using System.IO;
+
+namespace Automation.Functions.CodeGeneration.AddNugetConfig
+{
+    public class Process : IFunction
+    {
+        public Func<dynamic, dynamic, dynamic> Function { get; }
+            = (state, process) =>
+            {
+                var path = (string)process["path"];
+                if (!path.EndsWith("nuget.config"))
+                {
+                    path = Path.Combine(path, "nuget.config");
+                }
+                if (!File.Exists(path))
+                {
+                    var key = Guid.NewGuid().ToString();
+                    state[key] = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <packageSources>
+    <add key=""NuGet v3 feed"" value=""https://api.nuget.org/v3/index.json"" />
+  </packageSources>
+</configuration>";
+                    state = new WriteFile.Process().Function(state, new { key = key, path = path });
+                    state[key] = null;
+                }
+                return state;
+            };
+    }
+}
