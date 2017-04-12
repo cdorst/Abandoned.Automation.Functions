77ad06d2823cd3d9845204ae8af87dab610f6978
Chris Dorst <chris@dorst.email>
2017-04-12 06:07:19 -0500

    Adding AddNugetReference function

---
+++ Automation.Functions.CodeGeneration.AddNugetReference/Process.cs
@@ -0,0 +1,16 @@
+﻿using Automation.Functions.Abstractions;
+using System;
+
+namespace Automation.Functions.CodeGeneration.AddNugetReference
+{
+    public class Process : IFunction
+    {
+        public Func<dynamic, dynamic, dynamic> Function { get; }
+            = (state, process) => new FileFindAndReplace.Process().Function(state, new
+            {
+                path = (string)process["csproj"],
+                oldText = "</Project>",
+                newText = $"<ItemGroup><PackageReference Include=\"{(string)process["package"]}\" Version=\"{(string)process["version"]}\" /></ItemGroup></Project>"
+            });
+    }
+}
